3ef98740538876c86f3266d1e3cffb04f8440256
Mike B Golda <cash4golda@gmail.com>
2017-09-02 23:09:32 -0400

    Create apostles.js

---
+++ apostles.js
@@ -0,0 +1,11 @@
+var apostles = 12;
+var square = function(x) {
+  return x * x;
+};
+
+var cube = function(y) {
+  return y * y * y;
+};
+
+console.log(square(apostles));
+console.log(cube(apostles));
